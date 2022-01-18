cda94db12da46486ea0697d4adf5aa4f4e720d75
hafizhMo <hafizh.hizb@gmail.com>
2022-01-19 06:05:50 +0700

    types of operators.

--- app/src/main/java/com/hafizhmo/introrxjava/MainActivity.java
+++ app/src/main/java/com/hafizhmo/introrxjava/MainActivity.java
@@ -1,14 +1,45 @@
 package com.hafizhmo.introrxjava;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+
+        Observable.fromArray(numbers)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new DisposableObserver<Integer>() {
+                    @Override
+                    public void onNext(@NonNull Integer integer) {
+                        Log.d(TAG, "Number: " + integer);
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        Log.d(TAG, "onError: " + e.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "All numbers emitted!");
+                    }
+                });
     }
 }
