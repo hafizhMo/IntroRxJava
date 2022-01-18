d49f731b657a8d1e419ec19669d1e88622874cc9
hafizhMo <hafizh.hizb@gmail.com>
2022-01-18 16:20:23 +0700

    custom data type, operators.

--- app/src/main/java/com/hafizhmo/introrxjava/MainActivity.java
+++ app/src/main/java/com/hafizhmo/introrxjava/MainActivity.java
@@ -6,7 +6,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
@@ -20,121 +25,74 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    /**
-     * CompositeDisposable: Can maintain list of subscriptions in a pool and can dispose
-     * them all at once. Usually we call compositeDisposable.clear() in onDestroy() method,
-     * but you can call anywhere in the code.
-     */
-    private final CompositeDisposable compositeDisposable = new CompositeDisposable();
+    private final CompositeDisposable disposable = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Observable<String> animalsObservable = getAnimalsObservable();
-
-        DisposableObserver<String> animalsObserver = getAnimalsObserver();
-
-        DisposableObserver<String> animalsObserverAllCaps = getAnimalsAllCapsObserver();
-
-        /**
-         * filter() is used to filter out the animal names starting with `b`
-         * */
-        compositeDisposable.add(
-                animalsObservable
-                        .subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .filter(new Predicate<String>() {
-                            @Override
-                            public boolean test(@NonNull String s) throws Exception {
-                                return s.toLowerCase().startsWith("b");
-                            }
-                        })
-                        .subscribeWith(animalsObserver)
-        );
-
-        /**
-         * filter() is used to filter out the animal names starting with 'c'
-         * map() is used to transform all the characters to UPPER case
-         * */
-        compositeDisposable.add(
-                animalsObservable
-                        .subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .filter(new Predicate<String>() {
-                            @Override
-                            public boolean test(@NonNull String s) throws Exception {
-                                return s.toLowerCase().startsWith("c");
-                            }
-                        })
-                        .map(new Function<String, String>() {
-                            @Override
-                            public String apply(@NonNull String s) throws Exception {
-                                return s.toUpperCase();
-                            }
-                        })
-                        .subscribeWith(animalsObserverAllCaps)
+        disposable.add(getNotesObservable()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .map(new Function<Note, Note>() {
+                    @Override
+                    public Note apply(@NonNull Note note) throws Exception {
+                        note.setNote(note.getNote().toUpperCase());
+                        return note;
+                    }
+                })
+                .subscribeWith(getNotesObserver())
         );
     }
 
-    private DisposableObserver<String> getAnimalsObserver() {
-        return new DisposableObserver<String>() {
-
+    private DisposableObserver<Note> getNotesObserver() {
+        return new DisposableObserver<Note>() {
             @Override
-            public void onNext(String s) {
-                Log.d(TAG, "Name: " + s);
+            public void onNext(@NonNull Note note) {
+                Log.d(TAG, "Id: " + note.getId() + ", Note: " + note.getNote());
             }
 
             @Override
-            public void onError(Throwable e) {
-                Log.e(TAG, "onError: " + e.getMessage());
+            public void onError(@NonNull Throwable e) {
+                Log.d(TAG, "onError: " + e.getMessage());
             }
 
             @Override
             public void onComplete() {
-                Log.d(TAG, "All items are emitted!");
+                Log.d(TAG, "All notes are emitted!");
             }
         };
     }
 
-    private DisposableObserver<String> getAnimalsAllCapsObserver() {
-        return new DisposableObserver<String>() {
-
+    private Observable<Note> getNotesObservable() {
+        final List<Note> notes = prepareNotes();
+        return Observable.create(new ObservableOnSubscribe<Note>() {
             @Override
-            public void onNext(String s) {
-                Log.d(TAG, "Name: " + s);
-            }
+            public void subscribe(@NonNull ObservableEmitter<Note> emitter) throws Exception {
+                for (Note note: notes){
+                    if (!emitter.isDisposed()) emitter.onNext(note);
+                }
 
-            @Override
-            public void onError(Throwable e) {
-                Log.e(TAG, "onError: " + e.getMessage());
+                if (!emitter.isDisposed()) emitter.onComplete();
             }
-
-            @Override
-            public void onComplete() {
-                Log.d(TAG, "All items are emitted!");
-            }
-        };
+        });
     }
 
-    private Observable<String> getAnimalsObservable() {
-        return Observable.fromArray(
-                "Ant", "Ape",
-                "Bat", "Bee", "Bear", "Butterfly",
-                "Cat", "Crab", "Cod",
-                "Dog", "Dove",
-                "Fox", "Frog");
+    private List<Note> prepareNotes() {
+        List<Note> notes = new ArrayList<>();
+        notes.add(new Note(1, "buy tooth paste!"));
+        notes.add(new Note(2, "call brother!"));
+        notes.add(new Note(3, "watch narcos tonight!"));
+        notes.add(new Note(4, "pay power bill!"));
+
+        return notes;
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        /**
-         * Can maintain list of subscriptions in a pool and can dispose them all at once.
-         * Usually we call compositeDisposable.clear() in onDestroy() method,
-         * but you can call anywhere in the code.
-         */
-        compositeDisposable.clear();
+        disposable.clear();
     }
+
 }
