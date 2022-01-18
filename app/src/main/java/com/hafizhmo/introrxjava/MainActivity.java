eaa0596155e1a6cc411aa2eda616300b2f366a25
hafizhMo <hafizh.hizb@gmail.com>
2022-01-19 06:11:35 +0700

    chaining of operators.

--- app/src/main/java/com/hafizhmo/introrxjava/MainActivity.java
+++ app/src/main/java/com/hafizhmo/introrxjava/MainActivity.java
@@ -7,7 +7,11 @@ import android.os.Bundle;
 import android.util.Log;
 
 import io.reactivex.Observable;
+import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
 import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 
@@ -23,10 +27,27 @@ public class MainActivity extends AppCompatActivity {
         Observable.range(1, 20)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new DisposableObserver<Integer>() {
+                .filter(new Predicate<Integer>() {
                     @Override
-                    public void onNext(@NonNull Integer integer) {
-                        Log.d(TAG, "Number: " + integer);
+                    public boolean test(@NonNull Integer integer) throws Exception {
+                        return integer % 2 == 0;
+                    }
+                })
+                .map(new Function<Integer, String>() {
+                    @Override
+                    public String apply(@NonNull Integer integer) throws Exception {
+                        return integer + " is even number";
+                    }
+                })
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(@NonNull String s) {
+                        Log.d(TAG, "onNext: " + s);
                     }
 
                     @Override
