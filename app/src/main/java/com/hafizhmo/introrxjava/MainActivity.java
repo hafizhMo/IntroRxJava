a4f3ad6d157ace176fec37705f39488cf6a6e342
hafizhMo <hafizh.hizb@gmail.com>
2022-01-18 16:08:02 +0700

    multiple observers and composite disposable.

--- app/src/main/java/com/hafizhmo/introrxjava/MainActivity.java
+++ app/src/main/java/com/hafizhmo/introrxjava/MainActivity.java
@@ -9,8 +9,11 @@ import android.util.Log;
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
 import io.reactivex.functions.Predicate;
+import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,73 +21,120 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
     /**
-    * Disposable is used to dispose the subscription when an Observer no longer
-    * wants to listen to Observable. In android disposable are very useful in
-    * avoiding memory leaks. */
-    private Disposable disposable;
+     * CompositeDisposable: Can maintain list of subscriptions in a pool and can dispose
+     * them all at once. Usually we call compositeDisposable.clear() in onDestroy() method,
+     * but you can call anywhere in the code.
+     */
+    private final CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // observable.
         Observable<String> animalsObservable = getAnimalsObservable();
 
-        //observer.
-        Observer<String> animalsObserver = getAnimalsObserver();
+        DisposableObserver<String> animalsObserver = getAnimalsObserver();
 
-        // observer subscribing to observable.
-        animalsObservable
-                .observeOn(Schedulers.io()) // This tells the Observer to receive the data on android UI thread so that you can take any UI related actions.
-                .subscribeOn(AndroidSchedulers.mainThread()) // This tell the Observable to run the task on a background thread.
-                .filter(new Predicate<String>() { // operator filters the data by applying a conditional statement. The data which meets the condition will be emitted and the remaining will be ignored.
-                    @Override
-                    public boolean test(@NonNull String s) throws Exception {
-                        return s.toLowerCase().startsWith("y");
-                    }
-                })
-                .subscribeWith(animalsObserver);
+        DisposableObserver<String> animalsObserverAllCaps = getAnimalsAllCapsObserver();
 
+        /**
+         * filter() is used to filter out the animal names starting with `b`
+         * */
+        compositeDisposable.add(
+                animalsObservable
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .filter(new Predicate<String>() {
+                            @Override
+                            public boolean test(@NonNull String s) throws Exception {
+                                return s.toLowerCase().startsWith("b");
+                            }
+                        })
+                        .subscribeWith(animalsObserver)
+        );
+
+        /**
+         * filter() is used to filter out the animal names starting with 'c'
+         * map() is used to transform all the characters to UPPER case
+         * */
+        compositeDisposable.add(
+                animalsObservable
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .filter(new Predicate<String>() {
+                            @Override
+                            public boolean test(@NonNull String s) throws Exception {
+                                return s.toLowerCase().startsWith("c");
+                            }
+                        })
+                        .map(new Function<String, String>() {
+                            @Override
+                            public String apply(@NonNull String s) throws Exception {
+                                return s.toUpperCase();
+                            }
+                        })
+                        .subscribeWith(animalsObserverAllCaps)
+        );
     }
 
-    private Observer<String> getAnimalsObserver() {
-        return new Observer<String>() {
+    private DisposableObserver<String> getAnimalsObserver() {
+        return new DisposableObserver<String>() {
 
-            // Method will be called when an Observer subscribes to Observable.
             @Override
-            public void onSubscribe(@NonNull Disposable d) {
-                Log.d(TAG, "onSubscribe");
-                disposable = d;
-            }
-
-            // This method will be called when Observable starts emitting the data.
-            @Override
-            public void onNext(@NonNull String s) {
+            public void onNext(String s) {
                 Log.d(TAG, "Name: " + s);
             }
 
-            // In case of any error, onError() method will be called.
             @Override
-            public void onError(@NonNull Throwable e) {
-                Log.d(TAG, "onError: " + e.getMessage());
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: " + e.getMessage());
             }
 
-            // When an Observable completes the emission of all the items, onComplete() will be called.
             @Override
             public void onComplete() {
-                Log.d(TAG, "All members are emitted!");
+                Log.d(TAG, "All items are emitted!");
+            }
+        };
+    }
+
+    private DisposableObserver<String> getAnimalsAllCapsObserver() {
+        return new DisposableObserver<String>() {
+
+            @Override
+            public void onNext(String s) {
+                Log.d(TAG, "Name: " + s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "All items are emitted!");
             }
         };
     }
 
     private Observable<String> getAnimalsObservable() {
-        return Observable.fromArray("Yiren", "Aisha", "SiHyeon", "Onda", "Mia", "E:U");
+        return Observable.fromArray(
+                "Ant", "Ape",
+                "Bat", "Bee", "Bear", "Butterfly",
+                "Cat", "Crab", "Cod",
+                "Dog", "Dove",
+                "Fox", "Frog");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        disposable.dispose(); // calling disposable.dispose() in onDestroy() will un-subscribe the Observer.
+        /**
+         * Can maintain list of subscriptions in a pool and can dispose them all at once.
+         * Usually we call compositeDisposable.clear() in onDestroy() method,
+         * but you can call anywhere in the code.
+         */
+        compositeDisposable.clear();
     }
 }
