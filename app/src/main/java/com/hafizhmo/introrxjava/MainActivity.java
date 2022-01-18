f3d153bb8a8e4a431bac12dc16cee09c159b6c3a
hafizhMo <hafizh.hizb@gmail.com>
2022-01-18 15:23:27 +0700

    basic observable, observer

--- app/src/main/java/com/hafizhmo/introrxjava/MainActivity.java
+++ app/src/main/java/com/hafizhmo/introrxjava/MainActivity.java
@@ -1,11 +1,16 @@
 package com.hafizhmo.introrxjava;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
 
 import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,9 +21,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Observable<String> animalsObservable = Observable.just("Ant", "Bee", "Cat", "Dog", "Fox");
+        Observable<String> animalsObservable = getAnimalsObservable();
 
-        Log.d(TAG, "\nAnimals Observable: " + animalsObservable.blockingLast());
+        Observer<String> animalsObserver = getAnimalsObserver();
+
+        animalsObservable
+                .observeOn(Schedulers.io())
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe(animalsObserver);
 
     }
+
+    private Observer<String> getAnimalsObserver() {
+        return new Observer<String>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                Log.d(TAG, "onSubscribe");
+            }
+
+            @Override
+            public void onNext(@NonNull String s) {
+                Log.d(TAG, "Name: " + s);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.d(TAG, "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "All members are emitted!");
+            }
+        };
+    }
+
+    private Observable<String> getAnimalsObservable() {
+        return Observable.just("Yiren", "Aisha", "SiHyeon", "Onda", "Mia", "E:U");
+    }
+
 }
