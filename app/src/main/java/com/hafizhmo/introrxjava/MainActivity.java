ca6c18aac0c0ec6a5bdfc40ba87f4a6466f01996
hafizhMo <hafizh.hizb@gmail.com>
2022-01-18 15:17:35 +0700

    a simple way to use observable.

--- app/src/main/java/com/hafizhmo/introrxjava/MainActivity.java
+++ app/src/main/java/com/hafizhmo/introrxjava/MainActivity.java
@@ -3,12 +3,22 @@ package com.hafizhmo.introrxjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.Observable;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable<String> animalsObservable = Observable.just("Ant", "Bee", "Cat", "Dog", "Fox");
+
+        Log.d(TAG, "\nAnimals Observable: " + animalsObservable.blockingLast());
+
     }
 }
